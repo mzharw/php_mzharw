86c53d9ebe57bf2819d5ee1bef01b9fe1c04fa4a
mzharw <mazhariwirasena@gmail.com>
2024-10-26 21:35:19 +0700

    fix: add formula

--- soal1.php
+++ soal1.php
@@ -3,7 +3,8 @@
 $jml = $_GET['jml'] ?? ''; // added null handler
 echo "<table border=1 style='border-collapse: collapse;'>\n"; // added border collapse styling
 for ($a = $jml; $a > 0; $a--) {
-    echo "<tr><td colspan='$jml'>TOTAL : $a</td></tr>"; // added cols total each rows
+    $total = $a * ($a + 1) / 2;
+    echo "<tr><td colspan='$jml'>TOTAL : $total</td></tr>"; // added cols total each rows
     echo "<tr>\n";
     for ($b = $a; $b > 0; $b--) {
         echo "<td>$b</td>";
